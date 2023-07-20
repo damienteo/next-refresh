247f1e467f37eb958b147f47c66916c5447c073a
Damien <damien.teo.ks@gmail.com>
2023-07-20 15:43:53 +0800

    Navigation

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,9 +1,23 @@
 import Link from "next/link";
 
+import { useRouter } from "next/router";
+
 export default function Client() {
+  const router = useRouter();
+
+  const handleLoadPage = () => {
+    // router.push("/clients/load/page");
+
+    router.push({
+      pathname: "/clients/[id]/[name]",
+      query: { id: "load", name: "page" },
+    });
+  };
   return (
     <div>
       <h1>Client Page</h1>
+
+      <button onClick={handleLoadPage}>Load Page</button>
       <ul>
         <li>
           <Link href="/clients/apple/pollo">apple</Link>
