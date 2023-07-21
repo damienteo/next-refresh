6e263c432baed6229a2ee40b7771219e9c2543d7
Damien <damien.teo.ks@gmail.com>
2023-07-21 15:16:24 +0800

    Add getStaticProps

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,23 +1,32 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Error404Page(props: {
+  products: { link: string; name: string }[];
+}) {
+  const { products } = props;
   return (
     <div>
       <h1>404 Page</h1>
       <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog</Link>
-        </li>
+        {products?.map((product: { link: string; name: string }) => (
+          <li key={product.link}>
+            <Link href={`/` + product.link}>{product.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        { name: "Porfolio", link: "portfolio" },
+        { name: "Clients", link: "clients" },
+        { name: "About", link: "about" },
+        { name: "Blog", link: "blog" },
+      ],
+    },
+  };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,35 @@
 import Link from "next/link";
+import path from "path";
+import fs from "fs/promises";
+import { json } from "stream/consumers";
 
-export default function Home() {
+export default function Home(props: {
+  products: { link: string; name: string }[];
+}) {
+  const { products } = props;
+  console.log({ products });
   return (
     <div>
       <h1>Hello world</h1>
       <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog</Link>
-        </li>
+        {products?.map((product: { link: string; name: string }) => (
+          <li key={product.link}>
+            <Link href={`/` + product.link}>{product.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData.toString());
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
