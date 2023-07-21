d4eff5e07f2c7abc82c88d3c5b2350c45161c5d8
Damien <damien.teo.ks@gmail.com>
2023-07-21 15:34:32 +0800

    Add options for getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,14 @@ import { json } from "stream/consumers";
 
 export default function Home(props: {
   products: { link: string; name: string }[];
+  count: number;
 }) {
   const { products } = props;
-  console.log({ products });
+
   return (
     <div>
       <h1>Hello world</h1>
+
       <ul>
         {products?.map((product: { link: string; name: string }) => (
           <li key={product.link}>
@@ -23,13 +25,25 @@ export default function Home(props: {
 }
 
 export async function getStaticProps() {
+  console.log("------ Re-validating ------");
   const filePath = path.join(process.cwd(), "data", "dummy.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
+
+  if (!data)
+    return {
+      redirect: { destination: "/clients" },
+    };
+
+  if (data.products.length === 0)
+    return {
+      notFound: true,
+    };
 
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
